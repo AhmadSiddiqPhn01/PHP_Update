3f6f9f513338057d655052cf42fb34c1728d5ea0
Siddiq <103922506+AhmadSiddiqPhn01@users.noreply.github.com>
2022-07-13 13:45:40 +0700

    Update proses_simpan.php

--- proses_simpan.php
+++ proses_simpan.php
@@ -20,17 +20,8 @@ $path = "images/".$fotobaru;
 // Proses upload
 if(move_uploaded_file($tmp, $path)){ // Cek apakah gambar berhasil diupload atau tidak
 	// Proses simpan ke Database
-	$query = "INSERT INTO siswa VALUES('".$nis."', '".$nama."', '".$jenis_kelamin."', '".$telp."', '".$alamat."', '".$fotobaru."')";
-	$sql = mysqli_query($connect, $query); // Eksekusi/ Jalankan query dari variabel $query
-
-	if($sql){ // Cek jika proses simpan ke database sukses atau tidak
-		// Jika Sukses, Lakukan :
-		header("location: index.php"); // Redirect ke halaman index.php
-	}else{
-		// Jika Gagal, Lakukan :
-		echo "Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database.";
-		echo "<br><a href='form_simpan.php'>Kembali Ke Form</a>";
-	}
+	mysqli_query($connect,"insert into siswa(nis,nama,jenis_kelamin,telp,alamat,foto) values ('$nis', '$nama', '$jenis_kelamin', '$telp', '$alamat', '$fotobaru')");
+	echo "<script>document.location.href = 'index.php';</script>";
 }else{
 	// Jika gambar gagal diupload, Lakukan :
 	echo "Maaf, Gambar gagal untuk diupload.";
